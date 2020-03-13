36846706356d4892e19b5296bbf61bc2b9fd4561
natalie.seeto <nataliejseeto@gmail.com>
2020-03-13 15:24:09 +0000

    add routes

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,92 @@
 const express = require('express')
 const router = express.Router()
+const Question = require('./models/Question') // includes our model
+
 
 // get all quiz questions
-router.get('/questions', (req, res) => {
-
+router.get('/questions', async (req, res) => {
+    try {
+        const questions = await Question.find()
+        return res.status(200).json(questions)
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 })
 
 // get one quiz question
-router.get('/questions/:id', (req, res) => {
+router.get('/questions/:id', async (req, res) => {
+    try {
+        const _id = req.params.id 
 
+        const question = await Question.findOne({_id})        
+        if(!question){
+            return res.status(404).json({})
+        }else{
+            return res.status(200).json(question)
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 })
 
 // create one quiz question
-router.post('/questions', (req, res) => {
+router.post('/questions', async (req, res) => {
+    try {
+        const { description } = req.body
+        const { alternatives } = req.body
 
+        const question = await Question.create({
+            description,
+            alternatives
+        })
+
+        return res.status(201).json(question)
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 })
 
 // update one quiz question
-router.put('/questions/:id', (req, res) => {
+router.put('/questions/:id', async (req, res) => {
+try {
+    const _id = req.params.id
+    const { description } = req.body
 
+    let question = await Question.findOne({_id})
+    
+    if(!question){
+        question = await Question.create({
+            description,
+            alternatives
+        })    
+        return res.status(201).json(question)
+    } else {
+        question.description = description
+        question.alternatives = alternatives
+        await question.save()
+        return res.status(200).json(question)
+    }
+
+} catch(error){
+    return res.status(500).json({"error unable to update":error})
+}
 })
 
 // delete one quiz question
-router.delete('/questions/:id', (req, res) => {
+router.delete('/questions/:id', async (req, res) => {
+    try {
+        const _id = req.params.id 
 
-})
+        const question = await Question.deleteOne({_id})
 
-// this one is just a test
-router.get('/', (req, res) => {
-    res.send('H3ll0 W0RlD')
+        if(question.deletedCount === 0){
+            return res.status(404).json()
+        }else{
+            return res.status(204).json()
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
 })
 
 module.exports = router
